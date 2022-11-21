06fde09a92785eb2fb17d9f65217d6e8eb3a503e
NOKnowitAll <iaroslav.botsman@gmail.com>
2022-11-21 16:26:26 +0200

    second commit

---
+++ DemoApp/Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine("This is test .NET programm for Azure");
+Console.WriteLine("(c) NOKnowitAll, 21-Nov-22");
